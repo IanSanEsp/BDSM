fc7a6d31eafff49f95751cacd9c41620b7483969
IanSanEsp <isancheze2300@alumno.ipn.mx>
2025-10-25 01:41:26 -0600

    a

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,7 +6,8 @@ export const db = await mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
-console.log("Conectado a MySQL correctamente");
+console.log("✅ Conectado a MySQL correctamente");
